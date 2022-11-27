4937846c8321ca93cd1892cad6d8f776921721e4
sairajchouhan <sairaj2119@gmail.com>
2022-11-27 11:11:53 +0530

    almost there I want 120 but the output is 122

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ train_dataset = NYUv2Dataset(
 test_dataset = NYUv2Dataset(
     train=False,
 )
+print("test dataset is =====================================", len(train_dataset))
+print("test dataset is =====================================", len(test_dataset))
 
 train_loader = torch.utils.data.DataLoader(
     train_dataset, batch_size=batch_size, shuffle=True
@@ -38,14 +40,45 @@ test_loader = torch.utils.data.DataLoader(
 class Bottleneck(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        self.bnorm = nn.BatchNorm2d(
-            out_channels, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
         )
-        self.relu = nn.ReLU()
+        self.bnorm = nn.BatchNorm2d(
+            in_channels, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True
+        )
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        return self.conv(self.relu(self.bnorm(x)))
+        x = self.bnorm(x)
+        x = self.relu(x)
+        x = self.conv(x)
+        return x
+
+
+class DeConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super().__init__()
+        self.bnorm = nn.BatchNorm2d(
+            in_channels, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True
+        )
+        self.relu = nn.ReLU(inplace=True)
+        self.deconv = nn.ConvTranspose2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+
+    def forward(self, x):
+        x = self.bnorm(x)
+        x = self.relu(x)
+        x = self.deconv(x)
+        return x
 
 
 # Define model
@@ -58,9 +91,42 @@ class Main(nn.Module):
             in_channels=2208, out_channels=512, kernel_size=1, stride=1, padding=1
         )
 
+        # 1st deconvolution layer
+        self.deconv1 = DeConv(
+            in_channels=512, out_channels=512, kernel_size=5, stride=2, padding=2
+        )
+        self.avgpool1 = nn.AvgPool2d(kernel_size=(1, 2), stride=1, padding=0)
+
+        # 2nd deconvolution layer
+        self.deconv2 = DeConv(
+            in_channels=512, out_channels=256, kernel_size=5, stride=2, padding=2
+        )
+        self.avgpool2 = nn.AvgPool2d(kernel_size=(2, 2), stride=1, padding=0)
+
+        # 3rd deconvolution layer
+        self.deconv3 = DeConv(
+            in_channels=256, out_channels=128, kernel_size=5, stride=2, padding=2
+        )
+        self.avgpool3 = nn.AvgPool2d(kernel_size=(2, 2), stride=1, padding=0)
+
+        # 4th deconvolution layer
+        self.deconv4 = DeConv(
+            in_channels=128, out_channels=128, kernel_size=5, stride=2, padding=2
+        )
+        self.avgpool4 = nn.AvgPool2d(kernel_size=(2, 2), stride=1, padding=0)
+
     def forward(self, x):
         x = self.densenet161(x)
-        # x = self.btl1(x)
+        x = self.btl1(x)
+        x = self.deconv1(x)
+        x = self.avgpool1(x)
+        x = self.deconv2(x)
+        x = self.avgpool2(x)
+        x = self.deconv3(x)
+        x = self.avgpool3(x)
+        x = self.deconv4(x)
+        x = self.avgpool4(x)
+
         return x
 
 
@@ -73,88 +139,38 @@ model = Main().to(device)
 # print(model.densenet161.features.norm5.weight.size())
 
 
-loss_fn = nn.CrossEntropyLoss()
+loss_fn = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
 
 
-epochs = 20
-steps = 0
-print_every = 20
-for e in range(epochs):
-    running_loss = 0
-    for images, labels in iter(train_loader):
-        inputs, targets = images, labels
-        steps += 1
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
 
-        # if torch.cuda.is_available():
-        #     inputs, targets = inputs.cuda(), labels.cuda()
-        inputs = inputs.to(device)
-        targets = labels.to(device)
+        # Compute prediction error
+        pred = model(X)
 
-        print("----------------------------------", inputs.size())
-        print("----------------------------------", targets.size())
+        # convert to 3d tensor
+        loss = loss_fn(pred, y)
+        print()
 
+        # Backpropagation
         optimizer.zero_grad()
-
-        output = model.forward(inputs)
-
-        targets = torch.argmax(targets, dim=0)
-        output = torch.argmax(output, dim=0)
-
-        loss = loss_fn(output, targets)
-
         loss.backward()
         optimizer.step()
 
-        running_loss += loss.item()
-        if steps % print_every == 0:
-            model.eval()
-            accuracy = 0
-            valid_loss = 0
-            for ii, (images, labels) in enumerate(test_loader):
-                inputs, labels = images, labels
-                with torch.no_grad():
-                    if torch.cuda.is_available():
-                        inputs, labels = inputs.cuda(), labels.cuda()
-
-                    output = model.forward(inputs)
-                    valid_loss += loss_fn(output, labels).item()
-                    ps = torch.exp(output).data
-                    equality = labels.data == ps.max(1)[1]
-                    accuracy += equality.type_as(torch.FloatTensor()).mean()
-            print(
-                "Epoch: {}/{}.. ".format(e + 1, epochs),
-                "Training Loss: {:.3f}.. ".format(running_loss / print_every),
-                "Test Loss: {:.3f}.. ".format(valid_loss / len(test_loader)),
-                "Test Accuracy: {:.3f}".format(accuracy / len(test_loader)),
-            )
-
-            running_loss = 0
-            model.train()
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-# def train(dataloader, model, loss_fn, optimizer):
-#     size = len(dataloader.dataset)
-#     model.train()
-#     for batch, (X, y) in enumerate(dataloader):
-#         X, y = X.to(device), y.to(device)
+for param in model.parameters():
+    print(type(param), param.size())
 
-#         # Compute prediction error
-#         pred = model(X)
-#         # loss = loss_fn(pred, y)
-
-#         # # Backpropagation
-#         # optimizer.zero_grad()
-#         # loss.backward()
-#         # optimizer.step()
-
-#         # if batch % 100 == 0:
-#         #     loss, current = loss.item(), batch * len(X)
-#         #     print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-
-# epochs = 5
-# for t in range(epochs):
-#     print(f"Epoch {t+1}\n-------------------------------")
-#     train(train_loader, model, loss_fn, optimizer)
-# print("Done!")
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(test_loader, model, loss_fn, optimizer)
+print("Done!")
